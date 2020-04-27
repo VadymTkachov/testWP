e3702c6f37550dc0d030e19427e8f5419c769e63
Tkachov Vadim Borisovich <tkachov.vadim@gmail.com>
2020-04-27 14:21:30 +0300

    WP: Set the unite theme

--- html/wp-config.php
+++ html/wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root');
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql');
+define( 'DB_HOST', 'mysql' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '6d8841ecb2c5b666d21e0d50c7028d17af92ba25');
-define( 'SECURE_AUTH_KEY',  '6a510baafdc64c09669ac76bc3af8954824925ca');
-define( 'LOGGED_IN_KEY',    '004694b3dbc2a2003a3d71db80e8e50ae3e4abca');
-define( 'NONCE_KEY',        '26ca94ed2ad48f9d4da3fa85ae1b26a42bc8a7c3');
-define( 'AUTH_SALT',        '7e93c5a331a3d6cf8ed1c81b2b5fcf3b28c159b2');
-define( 'SECURE_AUTH_SALT', '98c3bd70f78e61fd3af321758c8e4f3fce64e12d');
-define( 'LOGGED_IN_SALT',   '25f44f3a23a53bc212af47853724b3e80de369e5');
-define( 'NONCE_SALT',       '66502c245a08166200e1d40abf3a7c31736ed494');
+define( 'AUTH_KEY', '6d8841ecb2c5b666d21e0d50c7028d17af92ba25' );
+define( 'SECURE_AUTH_KEY', '6a510baafdc64c09669ac76bc3af8954824925ca' );
+define( 'LOGGED_IN_KEY', '004694b3dbc2a2003a3d71db80e8e50ae3e4abca' );
+define( 'NONCE_KEY', '26ca94ed2ad48f9d4da3fa85ae1b26a42bc8a7c3' );
+define( 'AUTH_SALT', '7e93c5a331a3d6cf8ed1c81b2b5fcf3b28c159b2' );
+define( 'SECURE_AUTH_SALT', '98c3bd70f78e61fd3af321758c8e4f3fce64e12d' );
+define( 'LOGGED_IN_SALT', '25f44f3a23a53bc212af47853724b3e80de369e5' );
+define( 'NONCE_SALT', '66502c245a08166200e1d40abf3a7c31736ed494' );
 
 /**#@-*/
 
@@ -64,6 +64,12 @@ define( 'NONCE_SALT',       '66502c245a08166200e1d40abf3a7c31736ed494');
  * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
+
+/**
+ * Site settings URL
+ */
+define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] );
+define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] );
 
 /**
  * For developers: WordPress debugging mode.
@@ -81,15 +87,15 @@ define( 'WP_DEBUG', false );
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
+    $_SERVER['HTTPS'] = 'on';
 }
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+    define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
